f6836eb992c16e2ffa045bf05290ec2bc3de74e8
Camila Rodrigues <camila0830@gmail.com>
2019-06-29 17:00:19 -0300

    Busca de usuarios inseridos no input

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-let input1 = document.querySelector('input#user1');
-let input2 = document.querySelector('input#user2');
+let botao = document.getElementById('lutar')
 
+botao.addEventListener('click', function () {
+    let input1 = document.getElementById('user1').value;
+    let input2 = document.getElementById('user2').value;
+    
+    let usuario1 = buscarUsuarioGit(input1)
+        .then(usuario => {
+            return usuario;
+        })
+        .catch(err => console.log(err))
 
+    let usuario2 = buscarUsuarioGit(input2)
+        .then(usuario => {
+            return usuario;
+        })
+        .catch(err => console.log(err))
+
+        console.log(input1, input2)
+})
 
 
 function buscarUsuarioGit(usuarioGit) {
-   let dados = fetch('https://api.github.com/users/' + usuarioGit)
-      .then(function (response) {
-         return response.json();
-      })
-      .then(function (usuario) {
-         return usuario;
-      })
-      .catch(function (error) {
-         log('Request failed', error)
-      });
+    let dados = fetch('https://api.github.com/users/' + usuarioGit)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (usuario) {
+            return usuario;
+        })
+        .catch(function (error) {
+            log('Request failed', error)
+        });
 
-   return dados;
+    return dados;
 }
 
 
-let usuario1 = buscarUsuarioGit("ernaneaugusto")
-   .then(usuario => {
-      return usuario;
-   })
-   .catch(err => console.log(err))
 
-let usuario2 = buscarUsuarioGit("camilaIT")
-   .then(usuario => {
-      return usuario;
-   })
-   .catch(err => console.log(err))
